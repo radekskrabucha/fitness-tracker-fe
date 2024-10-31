7ce4efc654358711c1ac7a93e6638a74373ad273
radekskrabucha <rskrabucha13@gmail.com>
2024-10-31 11:31:22 +0100

    added formatZodError util

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -1,40 +1,9 @@
-import { HTTPError } from 'ky'
-import { z } from 'zod'
+import type { ZodError } from 'zod'
 
-type HttpStatusCodesMappedErrors = Record<number, string>
-
-type GetErrorMessageArgs<T extends Error> = {
-  error: T
-  httpStatusCodesMappedErrors?: HttpStatusCodesMappedErrors
-  errorHandler?: (error: T) => string
-}
-
-export const getErrorMessage = <T extends Error>({
-  error,
-  errorHandler,
-  httpStatusCodesMappedErrors
-}: GetErrorMessageArgs<T>): string => {
-  if (errorHandler) {
-    return errorHandler(error)
-  }
-  if (error instanceof z.ZodError) {
-    return error.errors.map(error => error.message).join('\n')
-  }
-  if (
-    error instanceof HTTPError &&
-    error.response.status &&
-    httpStatusCodesMappedErrors &&
-    httpStatusCodesMappedErrors[error.response.status]
-  ) {
-    const mappedError = httpStatusCodesMappedErrors[error.response.status]
-
-    if (mappedError) {
-      return mappedError
-    }
-  }
-  if (error instanceof Error) {
-    return error.message
-  }
-
-  return 'Oops! Something went wrong.'
-}
+export const formatZodError = (error: ZodError) =>
+  error.errors
+    .map(err => {
+      const path = err.path.join(' > ') || 'root'
+      return `Error at "${path}": ${err.message}`
+    })
+    .join('\n')

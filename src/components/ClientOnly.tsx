a97d061abe817bade2400d9fa087b3fca52cbd09
radekskrabucha <rskrabucha13@gmail.com>
2024-11-04 17:12:05 +0100

    feat(ClientOnly): added server fallback component

--- src/components/ClientOnly.tsx
+++ src/components/ClientOnly.tsx
@@ -1,8 +1,16 @@
 import { isHydrated } from '@solid-primitives/lifecycle'
-import { createMemo, type FlowComponent } from 'solid-js'
+import { createMemo, type FlowComponent, type JSX } from 'solid-js'
+
+type ClientOnlyProps = {
+  serverFallbackPlaceholder?: JSX.Element
+}
 
 // This component will only render its children on the client
-export const ClientOnly: FlowComponent = props => {
-  const children = createMemo(() => isHydrated() && props.children)
+// with possible fallback component, useful to avoid layout shifts
+export const ClientOnly: FlowComponent<ClientOnlyProps> = props => {
+  const children = createMemo(() =>
+    isHydrated() ? props.children : props.serverFallbackPlaceholder
+  )
+
   return <>{children()}</>
 }

963e013a7b82e05358aa3c6fe141d476aef03c20
radekskrabucha <rskrabucha13@gmail.com>
2024-11-07 18:05:04 +0100

    feat(profile): added initial create fitness profile form

---
+++ src/features/profile/CreateFitnessProfilePage.tsx
@@ -0,0 +1,8 @@
+import { CreateFitnessProfileForm } from './components/CreateFitnessProfileForm'
+
+export const CreateFitnessProfilePage = () => (
+  <section class="layout-section flex-1 items-center justify-center gap-2 text-center">
+    <h1 class="mb-4 text-4xl font-bold">Create Fitness Profile</h1>
+    <CreateFitnessProfileForm />
+  </section>
+)

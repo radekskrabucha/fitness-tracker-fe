37009453165817851d9c0f3ca27be2871a76bb4c
radekskrabucha <rskrabucha13@gmail.com>
2024-11-07 10:51:47 +0100

    feat(profile): added change-password to profile route

---
+++ src/routes/profile/change-password.tsx
@@ -0,0 +1,11 @@
+import { SEOTitle } from '~/components/Seo'
+import { ChangePasswordPage } from '~/features/profile/ChangePasswordPage'
+
+const ChangePassword = () => (
+  <>
+    <SEOTitle title="Change Password" />
+    <ChangePasswordPage />
+  </>
+)
+
+export default ChangePassword

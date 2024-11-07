436455105343906b4fb31d7253261ee7192f2f1f
radekskrabucha <rskrabucha13@gmail.com>
2024-11-07 17:06:35 +0100

    feat(profile): added createFitnessProfile

--- src/features/profile/actions.ts
+++ src/features/profile/actions.ts
@@ -1,7 +1,15 @@
 import { authClient } from '~/lib/auth'
+import { fetchApiClient } from '~/lib/fetch'
+import type { CreateFitnessProfile, FitnessProfile } from '~/models/profile'
 import type { ChangePasswordRequest, EditUserRequest } from './types/request'
 
 export const changePassword = (req: ChangePasswordRequest) =>
   authClient.changePassword(req)
 
 export const editUser = (req: EditUserRequest) => authClient.updateUser(req)
+
+export const createFitnessProfile = (req: CreateFitnessProfile) =>
+  fetchApiClient<FitnessProfile>('/profile', {
+    method: 'POST',
+    body: req
+  })

65004b4390e0f9ef78c62956f783976694dadfe2
radekskrabucha <rskrabucha13@gmail.com>
2024-11-09 18:47:26 +0100

    feat(workout-plan): added prefetching for workout routes

--- src/routes/workout-plans/index.tsx
+++ src/routes/workout-plans/index.tsx
@@ -1,5 +1,15 @@
+import type { RouteDefinition } from '@solidjs/router'
+import { useQueryClient } from '@tanstack/solid-query'
 import { SEOTitle } from '~/components/Seo'
 import { WorkoutPlansPage } from '~/features/workoutPlans/WorkoutPlansPage'
+import { getWorkoutPlansQueryOptions } from '~/features/workoutPlans/actions'
+
+export const route: RouteDefinition = {
+  load: () => {
+    const queryClient = useQueryClient()
+    queryClient.prefetchQuery(getWorkoutPlansQueryOptions())
+  }
+}
 
 const WorkoutPlans = () => (
   <>

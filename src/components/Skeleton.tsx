69a1f54d8edf54469414f571227b0e670ad57cc8
radekskrabucha <rskrabucha13@gmail.com>
2024-11-14 18:58:53 +0100

    refactor(Skeleton): implement splitProps for optimized prop distribution in Skeleton component for solid-js compatibility

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -3,15 +3,20 @@ import {
   type SkeletonRootProps
 } from '@kobalte/core/skeleton'
 import type { Component } from 'solid-js'
+import { splitProps } from 'solid-js/types/server/index.js'
 import type { WithClass } from '~/types/common'
 import { cn } from '~/utils/styles'
 
-export const Skeleton: Component<WithClass<SkeletonRootProps>> = props => (
-  <KobalteSkeleton
-    {...props}
-    class={cn(
-      'flex-1 rounded-lg bg-black/5 data-[animate=true]:animate-pulse data-[visible=false]:bg-transparent',
-      props.class
-    )}
-  />
-)
+export const Skeleton: Component<WithClass<SkeletonRootProps>> = props => {
+  const [localProps, others] = splitProps(props, ['class'])
+
+  return (
+    <KobalteSkeleton
+      class={cn(
+        'flex-1 rounded-lg bg-black/5 data-[animate=true]:animate-pulse data-[visible=false]:bg-transparent',
+        localProps.class
+      )}
+      {...others}
+    />
+  )
+}

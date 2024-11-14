5a3ce0c71acb3a44ebfc0802e6efbc9bc172281e
radekskrabucha <rskrabucha13@gmail.com>
2024-11-14 18:57:41 +0100

    refactor(Popover): utilize splitProps for improved prop handling in PopoverContent component in solid-js architecture

--- src/components/Popover.tsx
+++ src/components/Popover.tsx
@@ -1,5 +1,6 @@
 import { Popover } from '@kobalte/core/popover'
 import type { ComponentProps, FlowComponent } from 'solid-js'
+import { splitProps } from 'solid-js/types/server/index.js'
 import type { WithClass } from '~/types/common'
 import { cn } from '~/utils/styles'
 import { cardVariants } from './Card'
@@ -19,15 +20,19 @@ type ContentProps = Pick<
 
 type PopoverProps = WithClass<ContentProps>
 
-export const PopoverContent: FlowComponent<PopoverProps> = props => (
-  <Popover.Content
-    {...props}
-    class={cn(
-      cardVariants(),
-      'popover-animation z-10 min-w-52 origin-[var(--kb-hovercard-content-transform-origin)] items-center gap-4 overflow-hidden rounded-lg px-6 py-4',
-      props.class
-    )}
-  />
-)
+export const PopoverContent: FlowComponent<PopoverProps> = props => {
+  const [localProps, others] = splitProps(props, ['class'])
+
+  return (
+    <Popover.Content
+      class={cn(
+        cardVariants(),
+        'popover-animation z-10 min-w-52 origin-[var(--kb-hovercard-content-transform-origin)] items-center gap-4 overflow-hidden rounded-lg px-6 py-4',
+        localProps.class
+      )}
+      {...others}
+    />
+  )
+}
 
 export { Popover }

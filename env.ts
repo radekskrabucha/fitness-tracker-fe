cebcce8157b182f3adfd553b5d67fd6a917d9325
radekskrabucha <rskrabucha13@gmail.com>
2024-11-15 09:48:45 +0100

    chore: format

--- env.ts
+++ env.ts
@@ -2,5 +2,5 @@ import { defineConfig, Schema } from '@julr/vite-plugin-validate-env'
 
 export default defineConfig({
   VITE_APP_BASE_URL: Schema.string({ format: 'url', tld: false }),
-  VITE_API_BASE_URL: Schema.string({ format: 'url', tld: false }),
+  VITE_API_BASE_URL: Schema.string({ format: 'url', tld: false })
 })

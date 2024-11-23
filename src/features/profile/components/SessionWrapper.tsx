f8bf8342e4f001d398fde053e1b45e32816052d6
radekskrabucha <rskrabucha13@gmail.com>
2024-11-23 14:52:38 +0100

    feat(SessionWrapper): extend props to include QueryBoundary fallbacks for improved session handling

--- src/features/profile/components/SessionWrapper.tsx
+++ src/features/profile/components/SessionWrapper.tsx
@@ -1,4 +1,5 @@
 import { createQuery } from '@tanstack/solid-query'
+import type { ComponentProps } from 'solid-js'
 import type { Component, JSXElement } from 'solid-js'
 import { QueryBoundary } from '~/components/QueryBoundary'
 import { getSessionQueryOptions } from '~/features/signIn/actions'
@@ -7,13 +8,21 @@ import { nonNullable } from '~/utils/common'
 
 type SessionWrapperProps = {
   children: (user: User) => JSXElement
-}
+} & Pick<
+  ComponentProps<typeof QueryBoundary>,
+  'loadingFallback' | 'noDataFallback' | 'errorFallback'
+>
 
 export const SessionWrapper: Component<SessionWrapperProps> = props => {
   const getSessionQuery = createQuery(getSessionQueryOptions)
 
   return (
-    <QueryBoundary query={getSessionQuery}>
+    <QueryBoundary
+      query={getSessionQuery}
+      noDataFallback={props.noDataFallback}
+      loadingFallback={props.loadingFallback}
+      errorFallback={props.errorFallback}
+    >
       {({ user }) =>
         props.children({
           ...user,

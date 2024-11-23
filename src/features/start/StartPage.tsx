312f57a77f46f8537ef2c80b8db0b32cb5580485
radekskrabucha <rskrabucha13@gmail.com>
2024-11-23 14:33:33 +0100

    feat(start-page): create StartPage component and route for user navigation

---
+++ src/features/start/StartPage.tsx
@@ -0,0 +1,15 @@
+import { Card } from '~/components/Card'
+
+export const StartPage = () => (
+  <>
+    <section class="layout-section flex-1 justify-center">
+      <Card class="gap-10">
+        <h1 class="mb-4 text-4xl font-bold">Start</h1>
+        <p class="text-lg text-current/50">
+          Start your journey with Fitness Tracker. Choose your workout plan and
+          start tracking your progress today.
+        </p>
+      </Card>
+    </section>
+  </>
+)
---
+++ src/routes/start.tsx
@@ -0,0 +1,11 @@
+import { SEOTitle } from '~/components/Seo'
+import { StartPage } from '~/features/start/StartPage'
+
+const Start = () => (
+  <>
+    <SEOTitle title="Start" />
+    <StartPage />
+  </>
+)
+
+export default Start

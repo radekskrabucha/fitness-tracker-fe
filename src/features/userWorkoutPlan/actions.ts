88631f26131b9ce80d34dd14708c8556ad5caaa2
radekskrabucha <rskrabucha13@gmail.com>
2024-11-23 13:14:35 +0100

    feat(user-workout-plan): add getUserWorkoutPlan functionality and related types to fetch individual workout plans

--- src/features/userWorkoutPlan/actions.ts
+++ src/features/userWorkoutPlan/actions.ts
@@ -1,6 +1,9 @@
 import { queryOptions } from '@tanstack/solid-query'
 import { fetchApiClient } from '~/lib/fetch'
-import type { GetUserWorkoutPlansResponse } from './types/response'
+import type {
+  GetUserWorkoutPlanResponse,
+  GetUserWorkoutPlansResponse
+} from './types/response'
 
 export const getUserWorkoutPlans = () =>
   fetchApiClient<GetUserWorkoutPlansResponse>('/user/workout-plans')
@@ -9,5 +12,15 @@ export const getUserWorkoutPlansQueryOptions = () =>
   queryOptions({
     queryKey: ['getUserWorkoutPlans'],
     queryFn: () => getUserWorkoutPlans(),
+    deferStream: true
+  })
+
+export const getUserWorkoutPlan = (id: string) =>
+  fetchApiClient<GetUserWorkoutPlanResponse>(`/user/workout-plans/${id}`)
+
+export const getUserWorkoutPlanQueryOptions = (id: string) =>
+  queryOptions({
+    queryKey: ['getUserWorkoutPlan', id],
+    queryFn: () => getUserWorkoutPlan(id),
     deferStream: true
   })
--- src/features/userWorkoutPlan/types/response.ts
+++ src/features/userWorkoutPlan/types/response.ts
@@ -1,3 +1,8 @@
-import type { WorkoutPlanWithWorkouts } from '~/models/workoutPlan'
+import type {
+  WorkoutPlanWithWorkouts,
+  WorkoutPlanWithWorkoutsWithDetails
+} from '~/models/workoutPlan'
 
 export type GetUserWorkoutPlansResponse = Array<WorkoutPlanWithWorkouts>
+
+export type GetUserWorkoutPlanResponse = WorkoutPlanWithWorkoutsWithDetails

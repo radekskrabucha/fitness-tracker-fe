bf13aede17d22aca6cd00d50ee44ad99cbf1f4d7
radekskrabucha <rskrabucha13@gmail.com>
2024-11-25 08:54:22 +0100

    refactor(UserWorkoutPlans): improve layout and add no data fallback for user workout plans display

--- src/features/userWorkoutPlan/UserWorkoutPlansPage.tsx
+++ src/features/userWorkoutPlan/UserWorkoutPlansPage.tsx
@@ -7,13 +7,11 @@ export const UserWorkoutPlansPage = () => (
   <>
     <section class="layout-section gap-12">
       <Header title="Your Workout Plans" />
-      <div class="grid grid-cols-2 gap-10 max-lg:grid-cols-1">
-        <ClientOnly>
-          <SessionWrapper>
-            {user => <UserWorkoutPlans userId={user.id} />}
-          </SessionWrapper>
-        </ClientOnly>
-      </div>
+      <ClientOnly>
+        <SessionWrapper>
+          {user => <UserWorkoutPlans userId={user.id} />}
+        </SessionWrapper>
+      </ClientOnly>
     </section>
   </>
 )
--- src/features/userWorkoutPlan/components/UserWorkoutPlans.tsx
+++ src/features/userWorkoutPlan/components/UserWorkoutPlans.tsx
@@ -1,5 +1,8 @@
 import { createQuery } from '@tanstack/solid-query'
 import { Index, type Component } from 'solid-js'
+import { buttonVariants } from '~/components/Button'
+import { Card } from '~/components/Card'
+import { Link } from '~/components/Link'
 import { QueryBoundary } from '~/components/QueryBoundary'
 import { InternalLink } from '~/config/app'
 import { WorkoutPlanCard } from '~/features/workoutPlans/components/WorkoutPlanCard'
@@ -15,17 +18,41 @@ export const UserWorkoutPlans: Component<UserWorkoutPlansProps> = props => {
   )
 
   return (
-    <QueryBoundary query={getUserWorkoutPlansQuery}>
+    <QueryBoundary
+      query={getUserWorkoutPlansQuery}
+      noDataFallback={<NoWorkoutPlans />}
+    >
       {data => (
-        <Index each={data}>
-          {workoutPlan => (
-            <WorkoutPlanCard
-              {...workoutPlan()}
-              href={InternalLink.userWorkoutPlan}
-            />
-          )}
-        </Index>
+        <div class="grid grid-cols-2 gap-10 max-lg:grid-cols-1">
+          <Index each={data}>
+            {workoutPlan => (
+              <WorkoutPlanCard
+                {...workoutPlan()}
+                href={InternalLink.userWorkoutPlan}
+              />
+            )}
+          </Index>
+        </div>
       )}
     </QueryBoundary>
   )
 }
+
+const NoWorkoutPlans = () => (
+  <Card class="gap-10">
+    <div class="flex flex-col gap-2">
+      <h3 class="text-lg font-bold text-current/80">
+        You don't have any workout plans yet.
+      </h3>
+      <p class="text-lg text-current/50">
+        Create your first workout plan and start tracking your progress.
+      </p>
+    </div>
+    <Link
+      href={InternalLink.workoutPlans}
+      class={buttonVariants({ variant: 'primary' })}
+    >
+      Explore workout plans
+    </Link>
+  </Card>
+)

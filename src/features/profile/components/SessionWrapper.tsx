44deb533709814c4f064ec2ae79501890b624109
radekskrabucha <rskrabucha13@gmail.com>
2024-11-25 08:09:32 +0100

    refactor(SessionWrapper): simplify user data handling and update User type dependency on authClient for better session management

--- src/features/profile/components/SessionWrapper.tsx
+++ src/features/profile/components/SessionWrapper.tsx
@@ -4,7 +4,6 @@ import type { Component, JSXElement } from 'solid-js'
 import { QueryBoundary } from '~/components/QueryBoundary'
 import { getSessionQueryOptions } from '~/features/signIn/actions'
 import type { User } from '~/models/user'
-import { nonNullable } from '~/utils/common'
 
 type SessionWrapperProps = {
   children: (user: User) => JSXElement
@@ -23,18 +22,7 @@ export const SessionWrapper: Component<SessionWrapperProps> = props => {
       loadingFallback={props.loadingFallback}
       errorFallback={props.errorFallback}
     >
-      {({ user }) =>
-        props.children({
-          ...user,
-          image: nonNullable(user.image) ? user.image : undefined,
-          banned: nonNullable(user.banned) ? user.banned : undefined,
-          banExpires: nonNullable(user.banExpires)
-            ? user.banExpires
-            : undefined,
-          role: nonNullable(user.role) ? user.role : undefined,
-          banReason: nonNullable(user.banReason) ? user.banReason : undefined
-        })
-      }
+      {({ user }) => props.children(user)}
     </QueryBoundary>
   )
 }

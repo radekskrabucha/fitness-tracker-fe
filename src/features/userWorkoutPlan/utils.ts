320c7ebe2c5915181685e9756eb6cfcf6cfb69d5
radekskrabucha <rskrabucha13@gmail.com>
2024-11-27 08:34:07 +0100

    feat(userWorkoutPlan): add utility functions for transforming workout forms and default values setup

---
+++ src/features/userWorkoutPlan/utils.ts
@@ -0,0 +1,43 @@
+import type { GetWorkoutPlanResponse } from '../workoutPlan/types/response'
+import type { Form } from './form/addWorkoutSessionForm'
+import type { AddWorkoutSessionRequest } from './types/request'
+
+type TransformFormToRequestParams = {
+  workout: Form
+  workoutPlanId: string
+  workoutId: string
+}
+
+export const transformFormToRequest = ({
+  workout: { exercises, ...workout },
+  workoutPlanId,
+  workoutId
+}: TransformFormToRequestParams): AddWorkoutSessionRequest => ({
+  workoutPlanId,
+  workoutId,
+  ...workout,
+  exercises: exercises.map(exercise => ({
+    exerciseId: exercise.id,
+    completed: exercise.completed,
+    notes: exercise.notes,
+    attributes: exercise.completed
+      ? exercise.attributes.map(attribute => ({
+          name: attribute.name,
+          value: attribute.value
+        }))
+      : []
+  }))
+})
+
+export const workoutToFormDefaultValues = (
+  exercises: GetWorkoutPlanResponse['workouts'][number]['exercises']
+): Form => ({
+  date: '',
+  duration: 60,
+  notes: '',
+  exercises: exercises.map(exercise => ({
+    ...exercise,
+    completed: false,
+    notes: ''
+  }))
+})
